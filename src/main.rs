d6b13d561dbe19fbf3504c8b2c4371b53b64fb85
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-14 20:56:28 +0000

    Write to file instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
     let width = 256;
     let height = 256;
-    println!("P3");
-    println!("{} {}", width, height);
-    println!("255");
+    let mut file = File::create("out.ppm").unwrap();
+
+    writeln!(file, "P3").unwrap();
+    writeln!(file, "{} {}", width, height).unwrap();
+    writeln!(file, "255").unwrap();
+
     for x in 0..width {
         for y in 0..height {
-            println!("{} {} {}", x, y, 128);
+            writeln!(file, "{} {} {}", x, y, 128).unwrap();
         }
     }
 }
