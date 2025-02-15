f36f41d3f8ffd2ae0f86ef25c3c38521866ddd4b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-15 22:42:10 +0000

    Move three spheres to exampels

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use weekend_ray_tracing::camera::CameraBuilder;
-use weekend_ray_tracing::color::{Color, BLUE, PURPLE};
+use weekend_ray_tracing::color::Color;
 use weekend_ray_tracing::hittable::{Hittable, Sphere};
 use weekend_ray_tracing::material::{Lambertian, Material, Metal};
 use weekend_ray_tracing::vec3::Vec3;
@@ -8,36 +8,17 @@ use weekend_ray_tracing::world::{Object, World};
 fn main() {
     let mut world = World::default();
 
+    let sphere = Object {
+        hittable: Hittable::Sphere(Sphere::new(Vec3::new(0.0, 0.1, -1.0), 0.5).unwrap()),
+        material: Material::Metal(Metal::new(Color::new(1.0, 0.6, 0.6).unwrap())),
+    };
     let ground = Object {
         hittable: Hittable::Sphere(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0).unwrap()),
-        material: Material::Lambertian(Lambertian::new(PURPLE.mul(0.5).unwrap())),
-    };
-    let center = Object {
-        hittable: Hittable::Sphere(Sphere::new(Vec3::new(0.0, 0.0, -1.2), 0.5).unwrap()),
-        material: Material::Lambertian(Lambertian::new(BLUE.mul(0.5).unwrap())),
-    };
-    let left = Object {
-        hittable: Hittable::Sphere(Sphere::new(Vec3::new(-1.0, 0.0, -0.85), 0.5).unwrap()),
-        // Reflects slightly more green
-        material: Material::Metal(Metal::new(Color::new(0.7, 1.0, 0.7).unwrap())),
-    };
-    let right = Object {
-        hittable: Hittable::Sphere(Sphere::new(Vec3::new(0.65, -0.25, -0.7), 0.25).unwrap()),
-        // Highly reflective, no tint
-        material: Material::Metal(Metal::new(Color::new(0.9, 0.9, 0.9).unwrap())),
+        material: Material::Lambertian(Lambertian::new(Color::new(0.5, 0.5, 0.5).unwrap())),
     };
 
+    world.objects.push(sphere);
     world.objects.push(ground);
-    world.objects.push(center);
-    world.objects.push(left);
-    world.objects.push(right);
-
-    // let camera = CameraBuilder::new(world)
-    //     .width(2000)
-    //     .samples_per_pixel(200)
-    //     .max_depth(100)
-    //     .build();
-
     let camera = CameraBuilder::new(world).build();
 
     camera.render_to_file("out.ppm");
