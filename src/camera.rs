2223a45b85c4e4ae3121d7adfa71f4113494a722
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-15 13:30:53 +0000

    Move camera calculations to constructor

--- src/camera.rs
+++ src/camera.rs
@@ -12,15 +12,41 @@ pub struct Camera {
     world: World,
     width: u64,
     height: u64,
+    camera_center: Vec3,
+    pixel00_loc: Vec3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
 }
 
 impl Camera {
     pub fn new(world: World, width: u64, aspect_ratio: f64) -> Self {
         let height = (width as f64 / aspect_ratio) as u64;
+
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (width as f64 / height as f64);
+
+        let focal_length = 1.0;
+        let camera_center = vec3::ORIGIN;
+
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        let pixel_delta_u = viewport_u / width as f64;
+        let pixel_delta_v = viewport_v / height as f64;
+
+        let viewport_upper_left =
+            camera_center - Vec3::new(0.0, 0.0, focal_length) - (viewport_u + viewport_v) * 0.5;
+
+        let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
+
         Self {
             world,
             width,
             height,
+            camera_center,
+            pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
         }
     }
 
@@ -42,30 +68,15 @@ impl Camera {
 
     pub fn render(&self) -> Vec<Color> {
         println!("Rendering...");
-        let focal_length = 1.0;
-        let viewport_height = 2.0;
-        let viewport_width = viewport_height * (self.width as f64 / self.height as f64);
-        let camera_center = vec3::ORIGIN;
-
-        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-
-        let pixel_delta_u = viewport_u / self.width as f64;
-        let pixel_delta_v = viewport_v / self.height as f64;
-
-        let viewport_upper_left =
-            camera_center - Vec3::new(0.0, 0.0, focal_length) - (viewport_u + viewport_v) * 0.5;
-
-        let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
-
         (0..self.height)
             .cartesian_product(0..self.width)
             .progress_count(self.width * self.height)
             .map(|(y, x)| {
-                let pixel_center =
-                    pixel00_loc + (pixel_delta_u * x as f64) + (pixel_delta_v * y as f64);
-                let ray_direction = pixel_center - camera_center;
-                let ray = Ray::new(camera_center, ray_direction);
+                let pixel_center = self.pixel00_loc
+                    + (self.pixel_delta_u * x as f64)
+                    + (self.pixel_delta_v * y as f64);
+                let ray_direction = pixel_center - self.camera_center;
+                let ray = Ray::new(self.camera_center, ray_direction);
                 self.color(&ray)
             })
             .collect()
