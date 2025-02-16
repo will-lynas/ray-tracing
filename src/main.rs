b996b19d8a37a9dc2b53984b0b66e24da859f524
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-16 13:08:04 +0000

    Add some better dielectrics

--- src/main.rs
+++ src/main.rs
@@ -31,17 +31,27 @@ fn main() {
     );
     let left = Object::new(
         Sphere::new(Vec3::new(-1.0, 0.0, -0.85), 0.5).unwrap(),
-        Dielectric::new(1.52),
+        Metal::new(Color::new(0.7, 1.0, 0.7).unwrap(), 0.0).unwrap(),
     );
     let right = Object::new(
         Sphere::new(Vec3::new(0.65, -0.25, -0.7), 0.25).unwrap(),
-        Metal::new(Color::new(0.9, 0.9, 0.9).unwrap(), 0.0).unwrap(),
+        Metal::new(Color::new(0.95, 0.95, 0.95).unwrap(), 0.0).unwrap(),
+    );
+    let front = Object::new(
+        Sphere::new(Vec3::new(-0.2, -0.35, -0.6), 0.15).unwrap(),
+        Dielectric::new(1.52),
+    );
+    let front_right = Object::new(
+        Sphere::new(Vec3::new(0.35, -0.4, -0.9), 0.1).unwrap(),
+        Dielectric::new(1.8),
     );
 
     world.objects.push(ground);
     world.objects.push(center);
     world.objects.push(left);
     world.objects.push(right);
+    world.objects.push(front);
+    world.objects.push(front_right);
 
     let camera = Builder::new(world)
         // ---
