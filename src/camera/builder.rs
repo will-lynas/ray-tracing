6cf9770db539e1ed3dab0f2ee9761265c0ddde99
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-20 00:16:45 +0000

    Remove some options from the builder

--- src/camera/builder.rs
+++ src/camera/builder.rs
@@ -9,9 +9,6 @@ pub struct Builder {
     world: World,
     width: u64,
     aspect_ratio: f64,
-    viewport_height: f64,
-    focal_length: f64,
-    camera_center: Vec3,
     samples_per_pixel: u64,
     max_depth: u64,
 }
@@ -22,9 +19,6 @@ impl Builder {
             world,
             width: 2000,
             aspect_ratio: 16.0 / 9.0,
-            viewport_height: 2.0,
-            focal_length: 1.0,
-            camera_center: vec3::ORIGIN,
             samples_per_pixel: 200,
             max_depth: 100,
         }
@@ -57,34 +51,22 @@ impl Builder {
         self
     }
 
-    pub fn viewport_height(mut self, viewport_height: f64) -> Self {
-        self.viewport_height = viewport_height;
-        self
-    }
-
-    pub fn focal_length(mut self, focal_length: f64) -> Self {
-        self.focal_length = focal_length;
-        self
-    }
-
-    pub fn camera_center(mut self, camera_center: Vec3) -> Self {
-        self.camera_center = camera_center;
-        self
-    }
-
     pub fn build(self) -> Camera {
+        let camera_center = vec3::ORIGIN;
+        let viewport_height = 2.0;
+        let focal_length = 1.0;
+
         let height = (self.width as f64 / self.aspect_ratio) as u64;
-        let viewport_width = self.viewport_height * (self.width as f64 / height as f64);
+        let viewport_width = viewport_height * (self.width as f64 / height as f64);
 
         let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-        let viewport_v = Vec3::new(0.0, -self.viewport_height, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
         let pixel_delta_u = viewport_u / self.width as f64;
         let pixel_delta_v = viewport_v / height as f64;
 
-        let viewport_upper_left = self.camera_center
-            - Vec3::new(0.0, 0.0, self.focal_length)
-            - (viewport_u + viewport_v) * 0.5;
+        let viewport_upper_left =
+            camera_center - Vec3::new(0.0, 0.0, focal_length) - (viewport_u + viewport_v) * 0.5;
 
         let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
@@ -92,7 +74,7 @@ impl Builder {
             world: self.world,
             width: self.width,
             height,
-            camera_center: self.camera_center,
+            camera_center,
             pixel00_loc,
             pixel_delta_u,
             pixel_delta_v,
