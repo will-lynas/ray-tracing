967f74f99de39c6a35ca13fa9cc768ba290dfc02
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-26 21:02:33 +0000

    Add back comments in main

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,56 @@ use ray_tracing::{
 
 fn main() {
     let mut world = World::default();
+
+    // Ground
     world.add(
         Sphere::new_static(Vec3::new(0.0, -100.5, -1.0), 100.0),
         Lambertian::new(Color::new(0.48, 0.73, 0.2)),
     );
+
+    // Sphere solid center blue
     world.add(
         Sphere::new_static(Vec3::new(0.0, 0.0, -1.2), 0.5),
         Lambertian::new(Color::new(0.1, 0.2, 0.5)),
     );
+
+    // Sphere glass left
+    // Outer
     world.add(
         Sphere::new_static(Vec3::new(-1.0, 0.0, -0.85), 0.5),
         Dielectric::new(1.5),
     );
+    // Inner
     world.add(
         Sphere::new_static(Vec3::new(-1.0, 0.0, -0.85), 0.4),
         Dielectric::new(1.0 / 1.5),
     );
+
+    // Sphere metal right
     world.add(
         Sphere::new_static(Vec3::new(0.65, -0.25, -0.7), 0.25),
         Metal::new(Color::new(0.95, 0.95, 0.95), 0.0),
     );
+
+    // Sphere metal left red tint
     world.add(
         Sphere::new_static(Vec3::new(-0.5, -0.25, -2.0), 0.25),
         Metal::new(Color::new(0.95, 0.6, 0.6), 0.0),
     );
+
+    // Sphere glass left
+    // Outer
     world.add(
         Sphere::new_static(Vec3::new(-0.1, -0.15, -0.35), 0.35),
         Dielectric::new(1.7),
     );
+    // Inner
     world.add(
         Sphere::new_static(Vec3::new(-0.1, -0.15, -0.4), 0.30),
         Dielectric::new(1.0 / 1.7),
     );
+
+    // Sphere solid red
     world.add(
         Sphere::new_static(Vec3::new(-0.6, -0.35, -0.4), 0.15),
         Lambertian::new(Color::new(0.5, 0.0, 0.0)),
