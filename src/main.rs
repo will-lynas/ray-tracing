c8b80bbdfaeb89d049be5c473871151bd8aef618
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-03-14 17:21:50 +0000

    Add scenes selector

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use clap::Parser;
+use clap::{
+    Parser,
+    ValueEnum,
+};
 use glam::Vec3A as Vec3;
 use ray_tracing::{
     camera::Builder,
@@ -21,16 +24,37 @@ use ray_tracing::{
     },
 };
 
+#[derive(Parser, ValueEnum, Clone, Default)]
+enum Scene {
+    #[default]
+    ManySpheres,
+    ManyBouncingSpheres,
+}
+
 #[derive(Parser)]
 struct Args {
     /// Enable draft mode for faster rendering
     #[arg(short, long)]
     draft: bool,
+    /// The scene to render
+    #[arg(short, long, default_value = "many-spheres")]
+    scene: Scene,
 }
 
 fn main() {
     let args = Args::parse();
+    let mut builder = match args.scene {
+        Scene::ManySpheres => many_spheres(),
+        Scene::ManyBouncingSpheres => many_bouncing_spheres(),
+    };
+    if args.draft {
+        builder = builder.draft();
+    }
+    let camera = builder.build();
+    camera.render_to_file();
+}
 
+fn many_spheres() -> Builder {
     let mut world = HittableList::default();
 
     let ground_material = Lambertian::new(CheckerTexture::new(
@@ -88,7 +112,7 @@ fn main() {
 
     let bvh = BvhNode::from_list(world);
 
-    let mut builder = Builder::new(bvh)
+    Builder::new(bvh)
         .width(2000)
         .samples_per_pixel(500)
         .max_depth(50)
@@ -97,12 +121,80 @@ fn main() {
         .look_at(Vec3::new(0.0, 0.0, 0.0))
         .vup(Vec3::Y)
         .defocus_angle(0.6)
-        .focus_dist(10.0);
+        .focus_dist(10.0)
+}
 
-    if args.draft {
-        builder = builder.draft();
+fn many_bouncing_spheres() -> Builder {
+    let mut world = HittableList::default();
+
+    let ground_material = Lambertian::new(CheckerTexture::new(
+        SolidColor::new(0.1, 0.01, 0.4),
+        SolidColor::new(0.9, 0.9, 0.9),
+        0.3,
+    ));
+    world.add(Sphere::new_static(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    ));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let center = Vec3::new(
+                a as f32 + 0.9 * fastrand::f32(),
+                0.2,
+                b as f32 + 0.9 * fastrand::f32(),
+            );
+            if (center - Vec3::new(4.0, 0.2, 0.0)).length() <= 0.9 {
+                continue;
+            }
+            let end = if fastrand::f32() < 0.5 {
+                center + Vec3::Y * random_range(&(0.0..0.5))
+            } else {
+                center
+            };
+
+            let radius = 0.2;
+            let choose_mat = fastrand::f32();
+            if choose_mat < 0.50 {
+                let albedo = Color(Vec3::random()) * Color(Vec3::random());
+                let sphere_material = Lambertian::new(SolidColor::new_from_color(albedo));
+                world.add(Sphere::new_start_end(center, end, radius, sphere_material));
+            } else if choose_mat < 0.70 {
+                let albedo = Color(Vec3::random_range(&(0.5..1.0)));
+                let fuzz = random_range(&(0.0..0.5));
+                let sphere_material = Metal::new(albedo, fuzz);
+                world.add(Sphere::new_start_end(center, end, radius, sphere_material));
+            } else {
+                let sphere_material = Dielectric::new(1.5);
+                world.add(Sphere::new_start_end(center, end, radius, sphere_material));
+            };
+        }
     }
 
-    let camera = builder.build();
-    camera.render_to_file();
+    let material1 = Dielectric::new(1.5);
+    world.add(Sphere::new_static(Vec3::new(0.0, 1.0, 0.0), 1.0, material1));
+
+    let material2 = Lambertian::new(SolidColor::new(0.0, 0.9, 0.2));
+    world.add(Sphere::new_static(
+        Vec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    ));
+
+    let material3 = Metal::new(Color::new(0.90, 0.90, 1.0), 0.0);
+    world.add(Sphere::new_static(Vec3::new(4.0, 1.0, 0.0), 1.0, material3));
+
+    let bvh = BvhNode::from_list(world);
+
+    Builder::new(bvh)
+        .width(2000)
+        .samples_per_pixel(500)
+        .max_depth(50)
+        .vertical_fov(20.0)
+        .look_from(Vec3::new(13.0, 2.0, 3.0))
+        .look_at(Vec3::new(0.0, 0.0, 0.0))
+        .vup(Vec3::Y)
+        .defocus_angle(0.6)
+        .focus_dist(10.0)
 }
